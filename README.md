3ad447c7a11e13cbb9a1cf2a2a4630836a0ca903
Steve Barrios <hsteve1992@yahoo.com>
2020-07-28 19:35:39 -0500

    First push

---
+++ README.md
@@ -0,0 +1,1 @@
+In this project I was given some designs that were to be implented on iOS and so here are the results.
--- Shape Maker/ViewController.swift
+++ Shape Maker/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var stackLandscapeView: UIStackView!
+    @IBOutlet weak var yellowLandscapeView: UIView!
+    @IBOutlet weak var purpleLandscapeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        if UIScreen.main.traitCollection.horizontalSizeClass == .regular && UIScreen.main.traitCollection.verticalSizeClass == .regular
+        {
+            if let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation.isPortrait, orientation {
+                
+                toggleLandscape()
+            }
+            else {
+                
+                togglePortrait()
+            }
+        }
+                        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        if self.traitCollection.verticalSizeClass == .regular && self.traitCollection.horizontalSizeClass == .regular {
+            
+            togglePortrait()
+            toggleLandscape()
+                
+        }
+        
+        super.viewWillTransition(to: size, with: coordinator)
     }
 
-
+    func togglePortrait() {
+        orangeView.isHidden.toggle()
+        blueView.isHidden.toggle()
+        stackView.isHidden.toggle()
+    }
+    
+    func toggleLandscape() {
+        stackLandscapeView.isHidden.toggle()
+        purpleLandscapeView.isHidden.toggle()
+        yellowLandscapeView.isHidden.toggle()
+    }
 }
 
